14415d34a0247062e0204af326ba1ad9c0537f1f
Ivo Meixner <ivo.meixner@se7en.cz>
2017-12-09 23:37:56 +0100

    Implemented a method for Location-to-Point conversion

---
+++ NtxBot/ExtensionMethods_Location.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using Lib_K_Relay.GameData;
+using Lib_K_Relay.Networking;
+using Lib_K_Relay.Networking.Packets;
+using Lib_K_Relay.Networking.Packets.DataObjects;
+using Lib_K_Relay.Networking.Packets.Server;
+using Lib_K_Relay.Networking.Packets.Client;
+
+namespace NtxBot
+{
+    public static partial class ExtensionMethods
+    {
+        public static Point ToPoint(this Location loc) => new Point((int)loc.X, (int)loc.Y);
+    }
+}
