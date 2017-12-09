03cc633e342ea388093d69dd17e0737927c5d7c4
Ivo Meixner <ivo.meixner@se7en.cz>
2017-12-10 00:13:15 +0100

    Implemented a basic method for safely moving around the map

--- NtxBot/AbyssBot.cs
+++ NtxBot/AbyssBot.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Drawing;
 using Lib_K_Relay.GameData;
 using Lib_K_Relay.Networking;
 using Lib_K_Relay.Networking.Packets;
 using Lib_K_Relay.Networking.Packets.DataObjects;
 using Lib_K_Relay.Networking.Packets.Server;
 using Lib_K_Relay.Networking.Packets.Client;
+using Lib_K_Relay.Utilities;
 
 namespace NtxBot
 {
@@ -16,15 +19,41 @@ namespace NtxBot
     {
         private Client client;
         private GameMap map;
+        private FlashClient flash;
 
-        public AbyssBot(Client client, GameMap map)
+        public AbyssBot(Client client, GameMap map, FlashClient flash)
         {
             this.client = client;
             this.map = map;
+            this.flash = flash;
         }
 
         public void Run()
         {
+            // TODO
+        }
+
+        public void MoveSafely(Location target)
+        {
+            MovementEngine me = new MovementEngine(client, flash);
+
+            FindShortestSafePath(target).ForEach(x =>
+            {
+                me.BeginMove(x);
+
+                do
+                {
+                    Thread.Sleep(100);
+                }
+                while (me.Moving);
+            });
+
+            PluginUtils.Log("Abyss Bot", "Done moving!");
+        }
+
+        private IEnumerable<Location> FindShortestSafePath(Location target)
+        {
+            return new SpatialAStar<GameMapTile, object>(map.Tiles).Search(client.PlayerData.Pos.ToPoint(), target.ToPoint(), null).Select(x => new Location(x.X, x.Y));
         }
     }
 }
