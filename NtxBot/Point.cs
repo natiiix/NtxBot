0d9efdcba6ce7b676c0f9da8e599b56b519967fd
Ivo Meixner <ivo.meixner@se7en.cz>
2017-12-10 16:20:29 +0100

    Added my own Point implementation for working with 2D int coordinates

---
+++ NtxBot/Point.cs
@@ -0,0 +1,20 @@
+﻿using Lib_K_Relay.Networking.Packets.DataObjects;
+
+namespace NtxBot
+{
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static explicit operator Point(Location a) => new Point((int)a.X, (int)a.Y);
+
+        public static explicit operator Location(Point a) => new Location(a.X, a.Y);
+    }
+}
